69899ecbccdd220b439f8504358d1e2b03f037fe
michael brooks <michaelbrooks6897@gmail.com>
2024-10-17 13:10:47 -0400

    Updating future template for projects

--- vue-portfolio/src/components/Projects.vue
+++ vue-portfolio/src/components/Projects.vue
@@ -10,11 +10,68 @@
       </v-icon>
       <h2 style="color: grey;">I have older projects, but I would like to fill this out with new projects that actually show off my skill set</h2>
     </div>
+    <!-- <div class="titleContainer">
+      <h1>Projects</h1>
+    </div>
+    <div class="projectsContainer">
+      <v-card
+        v-for="(card, i) in cards"
+        class="ma-4"
+        width="344px"
+        height="400"
+        :key="i"
+      >
+        <v-img
+          width="344px"
+          :src="card.img"
+          cover
+        >
+        </v-img>
+        <v-card-title>
+          {{ card.title }}
+        </v-card-title>
+        <v-card-subtitle>
+          {{ card.subtitle }}
+        </v-card-subtitle>
+        <v-card-text>
+          {{ card.text }}
+        </v-card-text>
+      </v-card>
+    </div> -->
   </div>
 </template>
 <script lang="ts">
 export default {
-
+  data() {
+    return {
+      cards: [
+        {
+          img: '',
+          title: 'Portfolio',
+          subtitle: 'Vue, Vuetify, TypeScript',
+          text : 'This is a project that does super cool things.. this is why it is cool!! blah blah blah blah blah blah blah. These are the technologies I used.... Blah blah blah blah blah. I hope you enjoyed me ted talk'
+        },
+        {
+          img: '',
+          title: 'Portfolio',
+          subtitle: 'Vue, Vuetify, TypeScript',
+          text : 'This is a project that does super cool things.. this is why it is cool!! blah blah blah blah blah blah blah. These are the technologies I used.... Blah blah blah blah blah. I hope you enjoyed me ted talk'
+        },
+        {
+          img: '',
+          title: 'Portfolio',
+          subtitle: 'Vue, Vuetify, TypeScript',
+          text : 'This is a project that does super cool things.. this is why it is cool!! blah blah blah blah blah blah blah. These are the technologies I used.... Blah blah blah blah blah. I hope you enjoyed me ted talk'
+        },
+        {
+          img: '',
+          title: 'Portfolio',
+          subtitle: 'Vue, Vuetify, TypeScript',
+          text : 'This is a project that does super cool things.. this is why it is cool!! blah blah blah blah blah blah blah. These are the technologies I used.... Blah blah blah blah blah. I hope you enjoyed me ted talk'
+        },
+      ],
+    }
+  }
 }
 </script>
 <style scoped lang="scss">
@@ -26,6 +83,17 @@ export default {
   align-items: center;
 }
 
+.projectsContainer {
+  height: 75vh;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  width: 80vw;
+  justify-content: space-evenly;
+  align-content: flex-start;
+  overflow: auto;
+}
+
 .secondaryContainer {
   height: fit-content;
   width: fit-content;
@@ -33,5 +101,13 @@ export default {
   flex-direction: column;
   align-items: center;
   text-align: center;
+}
+
+.titleContainer {
+  width: 100%;
+  text-align: center; 
+  position: absolute;
+  top: 5%;
+  right: 0;
 }
 </style>
